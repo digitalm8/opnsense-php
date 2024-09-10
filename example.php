c73c70b107d38d1ea8fc41958f9222bc6c5c3491
root <root@xvm019.omgpu.ru>
2024-09-10 14:24:09 +0600

    #1

---
+++ example.php
@@ -0,0 +1,27 @@
+#!/usr/bin/php
+<?php
+require __DIR__ . "/opnsense.php";
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+$HOST    = "192.168.4.189";
+$KEY     = 'hQaJfCcxdxxxWFryyyqq9zzzRxSmvo5OYT9nkBgP4z2QmF9pwoxUsKiNn275JjsBuxnn1k5YfC7AOifw';
+$SECRET  = 't/NJrea8hTO3xxxN4syyymtXzzzLsboKeM6x+8mS32XWhsk3o7RN1GDJHXQMJTuash9poxJkEwgxEGAW';
+$DEBUG   = 0;
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+$osa = new OpnSenseAPI($HOST, $KEY, $SECRET, $DEBUG);
+
+$opnVLANS = $osa->run('interfaces/vlan_settings/searchItem',[])['rows'];
+$IFS = $osa->run('interfaces/overview/interfacesInfo',[])['rows'];
+$res = $osa->run('interfaces/vlan_settings/addItem',[
+    "vlan" =>
+    [
+        'descr' => 'Interface 1',
+        'if' => 'ixl2',
+        'pcp' => '0',
+        'proto' => '',
+        'vlanif' => 'ixl2_vlan1',
+        'tag' => 1,
+    ]
+]);
+$res = $osa->run('interfaces/vlan_settings/reconfigure',[]);
---
+++ opnsense.php
@@ -0,0 +1,53 @@
+<?php
+
+class OpnSenseAPI {
+	protected $host;
+    protected $key;
+    protected $secret;
+
+    protected $debug        = 0;
+
+    protected $def_proto    = 'https://';
+	protected $def_endpoint = '/api';
+
+    public function OpnSenseAPI($host, $key, $secret, $debug = 0) {
+        $this->host   = $host;
+        $this->key    = $key;
+        $this->secret = $secret;
+        $this->debug  = $debug;
+    }
+
+    public function run($mcc, $data = array()) {
+        $url = $this->def_proto . $this->host . $this->def_endpoint . '/' . $mcc;
+        if ($this->debug)
+            print("URL: {$url}\n");
+
+        $ch = curl_init( $url );
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_USERPWD, $this->key . ':' . $this->secret);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        if (count($data)) {
+            $jdata = json_encode($data, JSON_UNESCAPED_UNICODE|JSON_FORCE_OBJECT);
+            if ($this->debug)
+                print("JSON: {$jdata}\n");
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $jdata);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json', 'Accept: application/json']);
+        }
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        $html = curl_exec($ch);
+        if ($html === false) {
+            $errno = curl_errno($ch);
+            $message = curl_strerror($errno);
+            throw new Exception( "cURL error ({$errno}): {$message}" );
+        }
+        //print_r(curl_getinfo($ch));
+        curl_close($ch);
+        if ($this->debug)
+            print_r(json_decode($html, true));
+        return json_decode($html, true);
+    }
+}
